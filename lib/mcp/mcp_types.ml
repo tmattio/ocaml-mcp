d91c45b60132ffb335efec70333ab7dc91b1dc18
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-08-12 14:00:16 +0200

    fix(mcp): Fix json format for request_id and progress_token
    These fields are expected to be json strings or ints, but they are not meant to
    be serialized in the protocol as representing an OCaml variant. For example:
    
    Expected serialization: { "request_id": 2 }
    Before the fix:         { "request_id": [ "Int", 2 ] }
    
    Signed-off-by: Mathieu Barbin <mathieu.barbin@gmail.com>

--- lib/mcp/mcp_types.ml
+++ lib/mcp/mcp_types.ml
@@ -8,8 +8,43 @@ module Protocol = struct
   let jsonrpc_version = "2.0"
 end
 
-type request_id = String of string | Int of int [@@deriving yojson]
-type progress_token = String of string | Int of int [@@deriving yojson]
+module String_or_int () = struct
+  type t = String of string | Int of int [@@deriving yojson]
+
+  let of_yojson = function
+    | `Int i -> Ok (Int i)
+    | `String s -> Ok (String s)
+    | json -> of_yojson json
+
+  let to_yojson = function String s -> `String s | Int i -> `Int i
+end
+
+include (
+  struct
+    module T = String_or_int ()
+
+    type request_id = T.t = String of string | Int of int
+
+    let request_id_of_yojson = T.of_yojson
+    let request_id_to_yojson = T.to_yojson
+  end :
+    sig
+      type request_id = String of string | Int of int [@@deriving yojson]
+    end)
+
+include (
+  struct
+    module T = String_or_int ()
+
+    type progress_token = T.t = String of string | Int of int
+
+    let progress_token_of_yojson = T.of_yojson
+    let progress_token_to_yojson = T.to_yojson
+  end :
+    sig
+      type progress_token = String of string | Int of int [@@deriving yojson]
+    end)
+
 type cursor = string [@@deriving yojson]
 
 module ErrorCode = struct
